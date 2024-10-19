f2572b35b89458bff4bdd30aa7baa423600fcca2
Lee Ryan <f73@gmx.com>
2024-10-19 14:00:21 +0200

    Fix Leader+bD

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -15,17 +15,11 @@ return {
 
           -- mappings seen under group name "Buffer"
           ["<Leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-          ["<Leader>bD"] = {
-            function()
-              require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
-            end,
-            desc = "Pick to close",
-          },
           ["s"] = { "<cmd>HopChar1<CR>", desc = "Hop to Any Character" },
-            -- Move current line / block with Alt-j/k ala vscode.
-            ["<A-j>"] = { "<Esc>:move .+1<CR>==" },
-            -- Move current line / block with Alt-j/k ala vscode.
-            ["<A-k>"] = { "<Esc>:move .-2<CR>==" },
+          -- Move current line / block with Alt-j/k ala vscode.
+          ["<A-j>"] = { "<Esc>:move .+1<CR>==" },
+          -- Move current line / block with Alt-j/k ala vscode.
+          ["<A-k>"] = { "<Esc>:move .-2<CR>==" },
           -- tables with the `name` key will be registered with which-key if it's installed
           -- this is useful for naming menus
           ["<Leader>b"] = { name = "Buffers" },
@@ -37,15 +31,15 @@ return {
           -- setting a mapping to false will disable it
           -- ["<esc>"] = false,
         },
-          o = {
-            ["s"] = { "<cmd>HopChar1<CR>", desc = "Copy, Delete to Any Character" },
-          },
+        o = {
+          ["s"] = { "<cmd>HopChar1<CR>", desc = "Copy, Delete to Any Character" },
+        },
 
-          v = {
-            ["s"] = { "<cmd>HopChar1<CR>", desc = "Copy, Delete to Any Character" },
-            -- Move selected line / block of text in visual mode
-            ["<A-j>"] = { ":move '>+1<CR>gv-gv" },
-            ["<A-k>"] = { ":move '<-2<CR>gv-gv" },
+        v = {
+          ["s"] = { "<cmd>HopChar1<CR>", desc = "Copy, Delete to Any Character" },
+          -- Move selected line / block of text in visual mode
+          ["<A-j>"] = { ":move '>+1<CR>gv-gv" },
+          ["<A-k>"] = { ":move '<-2<CR>gv-gv" },
         },
       },
     },
