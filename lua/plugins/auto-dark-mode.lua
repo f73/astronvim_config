bc71d7c40774ead62dc2a86a3bb9bead4cacf320
fd <nula@gmx.com>
2026-02-12 16:32:27 +0100

    Add auto-dark mode plugin

---
+++ lua/plugins/auto-dark-mode.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      update_interval = 3000, -- checks every 1 second
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+        vim.cmd "colorscheme catppuccin"
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+        vim.cmd "colorscheme catppuccin-latte"
+      end,
+    },
+  },
+}
